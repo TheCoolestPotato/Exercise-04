df717786ac3bfec204cb4cf7ab6744656ce932ba
TheCoolestPotato <pr_mireles@utexas.edu>
2025-02-05 12:50:51 -0600

    WIP update, pick_solution function created

--- Exercise 4 Code List.R
+++ Exercise 4 Code List.R
@@ -6,7 +6,14 @@ load_dictionary <- function(x) {
   read.csv(filename)}
 valid_list <- load_dictionary("https://raw.githubusercontent.com/difiore/ada-datasets/refs/heads/main/collins-scrabble-words-2019.txt")
 solution_list <- load_dictionary("https://raw.githubusercontent.com/difiore/ada-datasets/refs/heads/main/google-10000-english-usa-no-swears.txt")
-valid_solutions <- intersect(solution_list,valid_list)
-valid_solutions <- mutate(valid_solutions, nchar(valid_solutions$words))
-valid_solutions <- mutate(valid_solutions, rename(valid_solutions, char_num = "nchar(valid_solutions$words)"))
-five_char <- filter(valid_solutions, char_num == "5")
+pick_solution <- function(df){
+  solution_list <- df
+  valid_solutions <- intersect(solution_list,valid_list)
+  valid_solutions <- mutate(valid_solutions, nchar(valid_solutions$words))
+  valid_solutions <- mutate(valid_solutions, rename(valid_solutions, char_num = "nchar(valid_solutions$words)"))
+  five_letter_words <- filter(valid_solutions, char_num == "5")
+  answer <- sample(five_letter_words$words,1)
+  answer <- str_split(answer, "")
+  return(answer)
+}
+
